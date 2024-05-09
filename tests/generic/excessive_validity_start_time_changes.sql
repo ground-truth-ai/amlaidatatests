a78880e81419049446a62dbf640d4892cd494499
mbazeley <36193523+mbazeley@users.noreply.github.com>
2024-05-09 13:18:53 +0100

    Initial commit

---
+++ tests/generic/excessive_validity_start_time_changes.sql
@@ -0,0 +1,28 @@
+{% test excessive_validity_start_time_changes(model, column_name, PKs, threshold) %}
+
+with validation as (
+
+    select
+        count(distinct validity_start_time) as validity_start_time_changes
+
+    from {{ model }}
+
+    group by {{ PKs }}
+
+),
+
+validation_errors as (
+
+    select
+        1
+
+    from validation
+    -- if this is true, then even_field is actually odd!
+    where validity_start_time_changes > {{ threshold }}
+
+)
+
+select *
+from validation_errors
+
+{% endtest %}
