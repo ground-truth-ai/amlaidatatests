17a0ccf1dd654dcb0ae40baeb706b63ff4e3d3e4
Matthew Shipton <matthew.shipton@groundtruthai.net>
2024-07-18 13:19:22 +0100

    Add __init__.py imports

---
+++ src/amlaidatatests/schema/v1/__init__.py
@@ -0,0 +1,5 @@
+from .account_party_link import account_party_link_schema
+from .party import party_schema
+from .party_supplementary_data import party_supplementary_data_schema
+from .risk_case_event import risk_case_event_schema
+from .transaction import transaction_schema
