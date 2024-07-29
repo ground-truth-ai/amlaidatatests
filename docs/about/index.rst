031451e330958be1418f0fd897612834bdf709ba
Matthew Shipton <matthew.shipton@groundtruthai.net>
2024-07-29 10:32:56 +0100

    Add about page

---
+++ docs/about/index.rst
@@ -0,0 +1,22 @@
+=====
+About
+=====
+
+``amlaidatatests`` is developed by `GroundtruthAI <https://groundtruthai.net/>`_
+who specialize in Google's AML AI API.
+
+
+The source code is available `on github
+<https://github.com/ground-truth-ai/amlaidatatests/tree/main>`_.
+
+Feature requests and support
+----------------------------
+
+Please raise any feature requests or support issues on the project's `issues
+page on github <https://github.com/ground-truth-ai/amlaidatatests/issues>`_.
+
+Licence
+-------
+
+``amlaidatatests`` is `licensed under the Apache Licence, version 2.0
+<https://github.com/ground-truth-ai/amlaidatatests/blob/main/LICENSE.md>`_.
--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,4 @@
+from sphinxawesome_theme import ThemeOptions
 from sphinxawesome_theme.postprocess import Icons
 
 # Configuration file for the Sphinx documentation builder.
@@ -8,7 +9,7 @@ from sphinxawesome_theme.postprocess import Icons
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = "AML AI Datatests"
+project = "amlaidatatests"
 copyright = "2024, Groundtruth AI Ltd"
 author = "Groundtruth AI Ltd"
 
@@ -32,9 +33,20 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 html_theme = "sphinxawesome_theme"
 html_static_path = ["_static"]
 html_permalinks_icon = Icons.permalinks_icon
+html_context = {
+    "mode": "production",
+    "feedback_url": "https://github.com/kai687/sphinxawesome-theme/issues/new?title=Feedback",
+}
 
 rst_prolog = """
 .. include:: <s5defs.txt>
 
 """
 html_css_files = ["css/s4defs-roles.css"]
+
+theme_options = ThemeOptions(
+    show_prev_next=True,
+    awesome_external_links=True,
+    main_nav_links={"Docs": "/index", "About": "/about"},
+    extra_header_link_icons={},
+)
